d49e4a9d6eb6312de2bb5c061c16fe873c0b8ee1
Karen Roberts <karenroberts@Karens-MacBook-Pro.local>
2018-10-31 15:40:46 +0000

    Challenge 14 added test and code for @bikes array with 20 bike limit

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,13 +3,13 @@ require 'docking_station'
 describe DockingStation do
   describe '#dock bike' do
     it 'raises an error when full' do
-      subject.dock(Bike.new)
+      20.times { subject.dock(Bike.new) }
       expect { subject.dock(Bike.new) }.to raise_error('Docking station full')
     end
     it 'returns docked bikes' do
       bike = Bike.new
       subject.dock(bike)
-      expect(subject.bike).to eq bike
+      expect(subject.bikes).to include(bike)
     end
   end
 
