7adf985d114add853d09b061f9601ed12de18b5e
Al <al@Als-MacBook-Pro.local>
2018-10-31 15:15:11 +0000

    challenge 13 added test and code for 'Docking station full'

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,16 +1,16 @@
 require 'docking_station'
 
 describe DockingStation do
-
-  it 'docks something' do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
-  end
-
-  it 'returns docked bikes' do
-    bike = Bike.new
-    subject.dock(bike)
-    expect(subject.bike).to eq bike
+  describe '#dock bike' do
+    it 'raises an error when full' do
+      subject.dock(Bike.new)
+      expect { subject.dock(Bike.new) }.to raise_error('Docking station full')
+    end
+    it 'returns docked bikes' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.bike).to eq bike
+    end
   end
 
   describe '#release_bike' do
