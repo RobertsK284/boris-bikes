00d75f8494c09fe3f0898d64a7222aac2ebb9541
Karen Roberts <karenroberts@Karens-MacBook-Pro.local>
2018-10-31 14:57:38 +0000

    challenge 12 added test and code for 'no bike available'

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,16 +1,6 @@
 require 'docking_station'
 
 describe DockingStation do
-#  it 'responds to release_bike' do
-#    expect(subject).to respond_to :release_bike
-#  end
-
-  it 'releases working bikes' do
-    bike = Bike.new
-    subject.dock(bike)
-    bike = subject.release_bike
-    expect(bike).to be_working
-  end
 
   it 'docks something' do
     bike = Bike.new
@@ -24,13 +14,13 @@ describe DockingStation do
   end
 
   describe '#release_bike' do
-    it 'releases a bike' do
-      bike = Bike.new
-      subject.dock(bike)
-      expect(subject.release_bike).to eq bike
+    it 'releases working bikes' do
+      subject.dock(Bike.new)
+      bike = subject.release_bike
+      expect(bike).to be_working
     end
     it 'raises an error when there are no bikes available' do
-      expect { subject.release_bike }.to raise_error 'No bikes available'
+      expect { subject.release_bike }.to raise_error('No bikes available')
     end
   end
 end
